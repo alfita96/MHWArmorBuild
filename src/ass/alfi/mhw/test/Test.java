7f4e4a159504428ebbc3dc81d6f7fa28be3fb368
alfita96 <alfita96@gmail.com>
2018-03-01 17:58:22 +0100

    Issue fixing class

--- src/ass/alfi/mhw/test/Test.java
+++ src/ass/alfi/mhw/test/Test.java
@@ -1,13 +1,16 @@
 package ass.alfi.mhw.test;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileReader;
-import java.lang.reflect.Array;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
-import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-
-import ass.alfi.mhw.objects.Skill;
 
 public class Test {
 	final public static String itemsPath = "src\\resources\\lists\\items";
@@ -18,12 +21,11 @@ public class Test {
 	final public static String waistPath = "src\\resources\\lists\\waistgear.json";
 	final public static String feetPath = "src\\resources\\lists\\feetgear.json";
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		getNames();
-		
-		
+
 	}
-	
+
 	public static JSONObject parseJSON() {
 		JSONParser parser = new JSONParser();
 		JSONObject jsonObject = null;
@@ -35,12 +37,15 @@ public class Test {
 		return jsonObject;
 	}
 
-	public static void getNames() {
-		JSONObject jsonObject = parseJSON();
-		System.out.println(jsonObject.toJSONString());
-		System.out.println(jsonObject.get("Critical Element"));
-//		String[] jsonArray = (String[]) jsonObject.get("Poison Attack");
-//		System.out.println(jsonArray.toString());
+	public static void getNames() throws IOException {
+		String line = null;
+		line = Files.readAllLines(Paths.get(skillsPath)).get(2);
+		File tmp = File.createTempFile("tmp", "");
+
+		BufferedReader br = new BufferedReader(new FileReader(skillsPath));
+		BufferedWriter bw = new BufferedWriter(new FileWriter(tmp));
+		
+		bw.write("value1");
 	}
 
 }
